3e0739a905146341c2a7e5d8631d4bd5a135875b
zanminkian <hellozmj@qq.com>
2023-04-18 14:55:02 +0000

    feat: init with `vite`

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,9 +1,1 @@
-declare module '*.css' {
-  const css: Record<string, string>
-  export default css;
-}
-
-declare module '*.scss' {
-  const css: Record<string, string>
-  export default css;
-}
+/// <reference types="vite/client" />

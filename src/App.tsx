1834911ff3adf9f207cbd62190b0495cac397c77
zanminkian <hellozmj@qq.com>
2023-09-01 03:37:43 +0000

    chore: upgrade deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 export default function App() {
   return (
-    <div className="text-base p-1 border border-black border-solid bg-red-100">hello world</div>
+    <div className="border border-solid border-black bg-red-100 p-1 text-base">hello world</div>
   );
 }

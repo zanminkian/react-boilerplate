bdc3590a8c6e4580b6d5367f5a7d153ccdb10ce8
zanminkian <hellozmj@qq.com>
2023-09-02 11:32:26 +0000

    feat: update demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 export default function App() {
   return (
-    <div className="border border-solid border-black bg-red-100 p-1 text-base">hello world</div>
+    <div className="border border-solid border-black bg-red-100 p-1 text-center text-base">
+      <span className="hover:text-lg">hello world</span>
+    </div>
   );
 }

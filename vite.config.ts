1faef61c8e4f360689db35f8d976a7fd90f626d5
zanminkian <hellozmj@qq.com>
2023-09-27 09:24:17 +0000

    chore: issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
 
-/**
- * @type {import('vite').UserConfig}
- */
-export default {
+export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
-};
+});

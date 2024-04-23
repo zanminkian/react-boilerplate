98b77beb63bfc29eb2d19766dfd1b020ea0c8ae0
zanminkian <hellozmj@qq.com>
2024-04-23 20:48:13 +0000

    chore: finish

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable esm/required-exports */
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);

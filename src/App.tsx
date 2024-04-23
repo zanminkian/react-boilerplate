ec10fc1d099c099f6e05a0c60177a7421f5457ed
zanminkian <hellozmj@qq.com>
2024-04-23 20:48:13 +0000

    chore: finish

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export const App = () => (
+  <div className="border border-solid border-black bg-red-100 p-1 text-center text-base">
+    <span className="hover:text-lg">hello world</span>
+  </div>
+);
---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+/**
+ * This file is required by tailwindcss extension in vscode
+ */
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+} satisfies Config;

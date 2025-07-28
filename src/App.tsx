2476723d7e66de37a845e1884ce1e0cd0bc86727
zanminkian <hellozmj@qq.com>
2025-07-28 08:34:55 +0000

    feat: optimize content

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 export function App() {
   const [num, setNum] = useState(0);
   return (
-    <div className="border border-solid border-black bg-red-100 p-1 text-center text-base">
-      <div className="hover:text-lg">hello world: {num}</div>
+    <div className="border border-solid border-black bg-red-100 p-1 text-center text-base hover:bg-green-100">
+      <div>hello world: {num}</div>
       <button className="d-btn border-black" onClick={() => setNum(num + 1)}>
         Click me
       </button>

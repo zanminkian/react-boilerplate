565c0817bd4006ca0e328e772e065fdd11ff646b
zanminkian <hellozmj@qq.com>
2025-08-07 06:32:20 +0000

    refactor: optimize structure

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line esm/required-exports
+import { createElement } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
+
+const rootElement = globalThis.document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(createElement(App));
+}

cf42312de898aa98776fe5683a10c502b4b29bec
zanminkian <hellozmj@qq.com>
2026-02-25 03:52:01 +0000

    chore: upgrade deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
-import { createElement } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 
 const rootElement = globalThis.document.getElementById("root");
 if (rootElement) {
-  createRoot(rootElement).render(createElement(App));
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
 }

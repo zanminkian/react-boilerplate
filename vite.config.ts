233f385ea19adab5f3e3bd7dbabfcea08ef46c16
zanminkian <hellozmj@qq.com>
2026-04-20 04:23:23 +0000

    build: use new react compiler format

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
+import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import reactCompiler from "babel-plugin-react-compiler";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src",
   base: "./",
   build: { outDir: "../dist" },
-  plugins: [
-    react({
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
-    tailwindcss(),
-  ],
+  plugins: [react(), babel({ plugins: [reactCompiler] }), tailwindcss()],
 });

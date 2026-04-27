35ab661d2d7ce466ef8198ef2e1291730a44d89d
zanminkian <hellozmj@qq.com>
2026-04-27 13:19:26 +0000

    chore: update scripts

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export function App() {
   const [num, setNum] = useState(0);
   return (
     <div className="border border-solid border-black bg-red-100 p-1 text-center text-base hover:bg-green-100">
-      <div>hello world: {num}</div>
+      <div>💖 Hello World 你好：{num}</div>
       <button className="d-btn border-black" onClick={() => setNum(num + 1)}>
         Click me
       </button>
